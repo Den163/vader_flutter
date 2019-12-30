29ed561e4a0475a49a410d56b38adbc78947da39
Den163 <dabramov@starlabstudio.com>
2019-12-30 21:19:28 +0400

    Fixed bug in module injector, when di container assigned not correctly

--- lib/src/module_injector.dart
+++ lib/src/module_injector.dart
@@ -1,58 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vader_di/vader.dart';
-import 'package:meta/meta.dart';
 
-abstract class ModuleInjector extends StatefulWidget {
-  ModuleInjector({Key key}) : super(key: key);
-
+abstract class ModuleInjector extends StatelessWidget {
+  Widget get child;
   void configure(DiModule module);
-  Widget build(BuildContext context);
 
-
-  /// Note. Don't override this
-  // TODO When package will be updated to Flutter 1.1.12.xx
-  // add [@nonVirtual] modifier 
-  @override
-  _ModuleInjectorState createState() => _ModuleInjectorState();
-}
-
-@sealed
-class _ModuleInjectorState extends State<ModuleInjector> {
-  DiModule _parentModule;
-  DiModule _module;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    DiModule parentModule;
-
-    try {
-      parentModule = Provider.of(context);
-    } on ProviderNotFoundError {
-      parentModule = null;
-    }
-
-    if (parentModule != _parentModule || _module == null) {
-      _module?.dispose();
-
-      _module = new DiModule(parentModule?.container);
-      widget.configure(_module);
-    }
-  }
-
-  @override
-  void dispose() {
-    _module.dispose();
-    super.dispose();
-  }
+  ModuleInjector({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Provider.value(
-      value: _module,
-      child: widget.build(context),
+    return Provider<DiModule>(
+      create: (_) {
+        final parentContainer = _tryGetParentContainer(context);
+        final diContainer = new DiContainer(parentContainer);
+        final module = new DiModule(diContainer);
+        configure(module);
+        return module;
+      },
+      dispose: (context, module) => module.dispose(),
+      child: child,
     );
+  }
+
+  DiContainer _tryGetParentContainer(BuildContext context) {
+    try {
+      return Provider.of<DiModule>(context).container;
+    } catch (e) {
+      return null;
+    }
   }
 }
